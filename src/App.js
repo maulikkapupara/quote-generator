5ee24f5e2991bb42409c5888de4d021eea4bc2bd
Maulik Kapupara <91789733+maulikkapupara@users.noreply.github.com>
2022-12-27 18:15:45 +0530

    change button name and change default quotes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 const App = () => {
   // const [quote, setQuote] = useState("");
-  const [data, setData] = useState('');
+  const [data, setData] = useState({
+    text: "He is able who thinks he is able.",
+    author: "Buddha",
+  });
 
   const getquotes = async () => {
     const fetchdata = await fetch(`https://type.fit/api/quotes`);
     const response = await fetchdata.json();
 
     let randomnum = Math.floor(Math.random() * response.length);
-
-    setData(response[randomnum])
-
+    const resdata = response[randomnum];
+    const quotedata = {
+      text: resdata.text,
+      author: resdata.author,
+    };
+    setData(quotedata);
+    // console.log(response);
   };
 
   // getquotes()
   return (
     <>
       <div className="App">
-        <h1 >Quote Generator App</h1>
-        <div className='box'>
+        <h1>Quote Generator App</h1>
+        <div className="box">
           <p>{data.text}</p>
           <p>{data.author}</p>
-          <button onClick={getquotes} className='btn'>Click Heare</button>
+          <button onClick={getquotes} className="btn">
+            Click Here To Change
+          </button>
         </div>
       </div>
-
-    </>);
-
-}
+    </>
+  );
+};
 
 export default App;
